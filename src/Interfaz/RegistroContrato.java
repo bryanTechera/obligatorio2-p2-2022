2076187618d981ebbafcfdc1668f11db89ed3695
Martin17693 <m_lores@hotmail.com>
2022-06-09 21:13:26 -0300

    seleccion listas

--- src/Interfaz/RegistroContrato.java
+++ src/Interfaz/RegistroContrato.java
@@ -161,6 +161,7 @@ public class RegistroContrato extends javax.swing.JFrame implements Observer {
         setTitle("Registro de contrato");
         getContentPane().setLayout(null);
 
+        listClientes.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
         Clientes.setViewportView(listClientes);
 
         getContentPane().add(Clientes);
@@ -295,7 +296,6 @@ public class RegistroContrato extends javax.swing.JFrame implements Observer {
         getContentPane().add(panelAcciones);
         panelAcciones.setBounds(530, 470, 290, 90);
 
-        listaDepositos.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_INTERVAL_SELECTION);
         paneldepositos.setViewportView(listaDepositos);
 
         getContentPane().add(paneldepositos);
@@ -328,6 +328,7 @@ public class RegistroContrato extends javax.swing.JFrame implements Observer {
         getContentPane().add(jSeparator1);
         jSeparator1.setBounds(30, 383, 810, 10);
 
+        listEmpleados.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
         Empleados.setViewportView(listEmpleados);
 
         getContentPane().add(Empleados);
--- src/Interfaz/RegistroEmpleado.java
+++ src/Interfaz/RegistroEmpleado.java
@@ -215,6 +215,7 @@ public class RegistroEmpleado extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(this, "empleado registrado", "Registro de empleado", JOptionPane.INFORMATION_MESSAGE);
             txtCedula.setText("");
             txtDireccion.setText("");
+            txtAñoIngreso.setText("0");
             txtTelefono.setText("");
             txtNombre.setText("");
             this.dispose();
